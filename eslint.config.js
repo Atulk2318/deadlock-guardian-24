6989447cc0108ef0a2ef1014e67544b32790452c
Atul Kumar Gupta <atulkuma.gupta2024@lpu.in>
2026-04-30 14:46:07 +0530

    Add React-related ESLint plugins

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 import globals from "globals";
+#react
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
